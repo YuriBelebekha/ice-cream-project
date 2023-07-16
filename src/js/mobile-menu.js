f6d9e299d152021aac4505b28485bef0d3f93a8b
Yuri Belebekha <ybelebekha@gmail.com>
2023-07-16 19:03:57 +0300

    Added function closeMenu and styles to common.scss - scroll-behavior: smooth

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -3,12 +3,24 @@
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menu: document.querySelector('[data-menu]'),
+    menuItem: document.querySelectorAll('.mobile-menu-nav__item'),
+    section: document.querySelectorAll('section'),
   };
 
-  refs.openMenuBtn.addEventListener('click', toggleModal);
-  refs.closeMenuBtn.addEventListener('click', toggleModal);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.menuItem.forEach(item => { item.addEventListener('click', closeMenu) });
+  refs.section.forEach(section => { section.addEventListener('click', closeMenu) });
 
-  function toggleModal() {
+  function toggleMenu() {
     refs.menu.classList.toggle('is-open');
-  }
+  };
+
+  function closeMenu() {
+    if (!refs.menu.classList.contains('is-open')) {
+      return;
+    } else {
+      refs.menu.classList.remove('is-open');
+    };
+  };
 })();
